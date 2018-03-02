3ac1f6c5db4ec018e94ee70a97c00c5cf02a965a
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2018-03-02 15:02:02 +0900

    added project files

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Hiroshi Takemoto on 3/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
